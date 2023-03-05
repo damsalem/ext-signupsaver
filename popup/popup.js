d9060a4da2b39444943da9b3c32ae595a78202ee
Dani Amsalem <dani@theamsalems.com>
2023-03-05 14:44:59 -0500

    Refactor JS to use Async Await

--- popup/popup.js
+++ popup/popup.js
@@ -8,94 +8,105 @@ const changeText = document.querySelector("#changeText");
 
 saveSupButton.addEventListener("click", handleSaveSup);
 
-function handleSaveSup() {
-    // Get the current/active tab
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        const activeTab = tabs[0];
-        const tabTitle = activeTab.title;
-        const tabUrl = activeTab.url;
+async function handleSaveSup() {
+	// Get the current/active tab
+	const [activeTab] = await chrome.tabs.query({
+		active: true,
+		currentWindow: true,
+	});
 
-        manageBookmarks(tabTitle, tabUrl);
-    });
+	const activeTabTitle = activeTab.title;
+	const activeTabUrl = activeTab.url;
+
+	// Search or create folder
+	const folderId = await searchOrCreateSignUpSaverFolder();
+
+	// Create the bookmark
+	const bookmark = createBookmark(activeTabTitle, activeTabUrl, folderId);
+
+	// Query bookmarks for matches
+	getSignUpSaverBookmarks(folderId);
+
+	// Update popup unordered list
+
+	// Toggle change text in popup
+	toggleChangeText("addition");
 }
 
-function manageBookmarks(tabTitle, tabUrl) {
-    // Search for SignUpSaver folder
-    chrome.bookmarks.search(
-        { query: "SignUpSaver", title: "SignUpSaver" },
-        function (folder) {
-            const folderId = folder[0]?.id;
+/****
+ *
+ * Utility Functions
+ *
+ ****/
 
-            // If folder exists, create bookmark
-            if (folderId) {
-                createBookmark(tabTitle, tabUrl, folderId);
-                return;
-            }
+// Search for SignUpSaver folder
+async function getSignUpSaverFolderId(
+	query = "SignUpSaver",
+	title = "SignUpSaver"
+) {
+	const folder = await chrome.bookmarks.search({ query, title });
+	// If folder found, return the id, else return null
+	return folder.length > 0 ? folder[0].id : null;
+}
 
-            // Default, create SignUpSaver folder
-            createSignUpSaverFolder(tabTitle, tabUrl);
-        }
-    );
+// Create SignUpSaver folder
+async function createSignUpSaverFolderId(title = "SignUpSaver") {
+	const folder = await chrome.bookmarks.create({ title: title });
+	return folder.id;
+}
 
-    // Create SignUpSaver folder
-    function createSignUpSaverFolder(tabTitle, tabUrl) {
-        chrome.bookmarks.create(
-            {
-                title: "SignUpSaver",
-            },
-            function (folder) {
-                // Create first bookmark
-                createBookmark(tabTitle, tabUrl, folder.id);
-            }
-        );
-    }
+// Search for folder, if it doesn't exist, create it
+async function searchOrCreateSignUpSaverFolder() {
+	let folderId = await getSignUpSaverFolderId();
+	if (!folderId) {
+		folderId = await createSignUpSaverFolderId();
+	}
+	return folderId;
+}
 
-    // Create SignUpSaver bookmark
-    function createBookmark(tabTitle, tabUrl, folderId) {
-        chrome.bookmarks.create(
-            {
-                title: tabTitle,
-                url: tabUrl,
-                parentId: folderId,
-                index: 0,
-            },
-            function () {
-                addToBookmarkList(tabTitle);
-            }
-        );
+// Search for bookmarks within the folder
+async function getSignUpSaverBookmarks(id) {
+	return await chrome.bookmarks.getChildren(id);
+}
 
-        toggleChangeText("addition");
-    }
+// Create SignUpSaver bookmark
+async function createBookmark(tabTitle, tabUrl, folderId) {
+	return await chrome.bookmarks.create({
+		title: tabTitle,
+		url: tabUrl,
+		parentId: folderId,
+		index: 0,
+	});
+}
 
-    // Toggle ChangeText
-    // parameters include: "addition", "removal", and "exists"
-    function toggleChangeText(change = "addition") {
-        switch (change) {
-            case "addition":
-                changeText.innerHTML = "Bookmark added";
-                break;
-            case "removal":
-                changeText.innerHTML = "Bookmark removed";
-                break;
-            case "exists":
-                changeText.innerHTML = "Bookmark already exists";
-                break;
-            default:
-                changeText.innerHTML = "Hmmm that didn't work";
-        }
-        changeText.style.visibility = "visible";
+// Toggle ChangeText
+// parameters include: "addition", "removal", and "exists"
+function toggleChangeText(change = "addition") {
+	switch (change) {
+		case "addition":
+			changeText.innerHTML = "Bookmark added";
+			break;
+		case "removal":
+			changeText.innerHTML = "Bookmark removed";
+			break;
+		case "exists":
+			changeText.innerHTML = "Bookmark already exists";
+			break;
+		default:
+			changeText.innerHTML = "Hmmm that didn't work";
+	}
+	changeText.style.visibility = "visible";
 
-        setTimeout(function hideText() {
-            changeText.style.visibility = "hidden";
-        }, 2000);
-    }
+	setTimeout(function hideText() {
+		changeText.style.visibility = "hidden";
+	}, 2000);
+}
 
-    // Add to Bookmark List
-    function addToBookmarkList(tabTitle) {
-        bookmarks.appendChild(
-            Object.assign(document.createElement("li"), {
-                innerHTML: tabTitle,
-            })
-        );
-    }
+// Add to Bookmark List
+function addToBookmarkList(tabTitle) {
+	bookmarks.appendChild(
+		Object.assign(document.createElement("li"), {
+			innerHTML: tabTitle,
+		})
+	);
 }
